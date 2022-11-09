a7982a7c7ba55fbb0589014528606c4263352fba
Rakibul Islam <rakibul241221@gmail.com>
2022-11-09 22:44:27 +0600

    react photoview implement on service photo

--- src/pages/Services/Services/ServiceItem/ServiceItem.js
+++ src/pages/Services/Services/ServiceItem/ServiceItem.js
@@ -1,16 +1,27 @@
 import React from 'react';
+import { PhotoProvider, PhotoView } from 'react-photo-view';
+import 'react-photo-view/dist/react-photo-view.css';
 import { Link } from 'react-router-dom';
+
 
 const ServiceItem = ({ service }) => {
     const { description, name, _id, img, price, rating } = service;
     return (
         <div className="card w-96 bg-base-100 shadow-lg p-3">
             <div >
-                <figure style={{ height: "250px" }}><img src={img} className="rounded" alt="Shoes" /></figure>
+                <figure style={{ height: "250px" }}>
+                    <PhotoProvider>
+                        <PhotoView src={img}>
+                            <img src={img} className="rounded" alt="Shoes" />
+                        </PhotoView>
+                    </PhotoProvider>
+                </figure>
             </div>
             <div className="card-body">
                 <h2 className="card-title">{name}</h2>
-                <p>{description.length > 100 ? description.slice(0, 100) : description}</p>
+                <p>
+                    {description.length > 100 ? description.slice(0, 100) : description}
+                </p>
                 <div className='flex justify-between'>
                     <span>Fee: ${price}</span>
                     <span>Rating: {rating}</span>
